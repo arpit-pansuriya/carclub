76c1347816738a0a92f53eebb4f6ddc8f1214d60
r0naldo007 <arpitpansuriya007@gmail.com>
2023-10-02 17:35:24 -0400

    fix bugs

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,13 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtoProps {
-  
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+}
+
+export interface SearchManufactureProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
 }
